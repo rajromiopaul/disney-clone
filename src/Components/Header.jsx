ec5fe9a5e6c1f07f1ea5bf7fc0d3ab6b2d7ccbac
Raj Romio Paul <rajromiopaul@gmail.com>
2026-01-10 15:09:44 +0530

    header added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import logo from './../assets/Images/logo.png'
 
 function Header() {
   return (
-    <div>Header</div>
+      <div>
+          <img src="" alt="" />
+    </div>
   )
 }
 
